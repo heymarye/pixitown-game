51be017a34d48493172e19cc8c46125a70c9aa51
heymarye <82950840+heymarye@users.noreply.github.com>
2022-11-21 20:40:36 +0200

    collisions.json added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
 
 canvas.width = 1024;
 canvas.height = 576;
@@ -7,88 +7,88 @@ canvas.height = 576;
 context.fillRect(0, 0, canvas.width, canvas.height);
 
 const mapImg = new Image();
-mapImg.src = '/img/pelletTown.png';
+mapImg.src = "/img/pelletTown.png";
 
 const playerImg = new Image();
-playerImg.src = '/img/playerDown.png';
+playerImg.src = "/img/playerDown.png";
 
-let lastKey = '';
+let lastKey = "";
 
 const keys = {
   w: {
-    pressed: false
+    pressed: false,
   },
   s: {
-    pressed: false
-  }, 
+    pressed: false,
+  },
   a: {
-    pressed: false
+    pressed: false,
   },
   d: {
-    pressed: false
+    pressed: false,
   },
   ArrowUp: {
-    pressed: false
+    pressed: false,
   },
   ArrowDown: {
-    pressed: false
+    pressed: false,
   },
   ArrowLeft: {
-    pressed: false
+    pressed: false,
   },
   ArrowRight: {
-    pressed: false
-  }
+    pressed: false,
+  },
 };
 
-window.addEventListener('keydown', ({ keyCode }) => {
-  switch(keyCode) {
-    case 87: //up 
+window.addEventListener("keydown", ({ keyCode }) => {
+  switch (keyCode) {
+    case 87: //up
       keys.w.pressed = true;
-      lastKey = 'w';
+      lastKey = "w";
       break;
     case 38:
       keys.ArrowUp.pressed = true;
-      lastKey = 'ArrowUp';
-      break; 
+      lastKey = "ArrowUp";
+      break;
     case 83: //down
       keys.s.pressed = true;
-      lastKey = 's';
-      break;    
+      lastKey = "s";
+      break;
     case 40:
       keys.ArrowDown.pressed = true;
-      lastKey = 'ArrowDown';
+      lastKey = "ArrowDown";
       break;
     case 65: //left
       keys.a.pressed = true;
-      lastKey = 'a';
+      lastKey = "a";
       break;
     case 37:
       keys.ArrowLeft.pressed = true;
-      lastKey = 'ArrowLeft';
+      lastKey = "ArrowLeft";
       break;
     case 68: //right
       keys.d.pressed = true;
-      lastKey = 'd';
+      lastKey = "d";
       break;
     case 39:
       keys.ArrowRight.pressed = true;
-      lastKey = 'ArrowRight';
+      lastKey = "ArrowRight";
       break;
   }
 });
 
-window.addEventListener('keyup', ({ keyCode }) => {
-  switch(keyCode) {
-    case 87: //up 
+window.addEventListener("keyup", ({ keyCode }) => {
+  switch (keyCode) {
+    case 87: //up
       keys.w.pressed = false;
       break;
     case 38:
       keys.ArrowUp.pressed = false;
-      break; 
+      break;
     case 83: //down
       keys.s.pressed = false;
-      break;    
+      break;
     case 40:
       keys.ArrowDown.pressed = false;
       break;
@@ -118,40 +118,49 @@ class Sprite {
   }
 }
 
-const map = new Sprite({ 
+const map = new Sprite({
   position: {
     x: -735,
-    y: -600
+    y: -600,
   },
-  image: mapImg
+  image: mapImg,
 });
 
 function animate() {
   window.requestAnimationFrame(animate);
   map.draw();
-  context.drawImage(playerImg,
+  context.drawImage(
+    playerImg,
     0, //x-coordinate to begin cropping from
     0, //y-coordinate to begin cropping from
     playerImg.width / 4, //crop width
     playerImg.height, //crop height
-    canvas.width / 2 - (playerImg.width / 4) / 2, //actual x-coordinate
+    canvas.width / 2 - playerImg.width / 4 / 2, //actual x-coordinate
     canvas.height / 2 - playerImg.height / 2, //actual y-coordinate
     playerImg.width / 4, //actual width
-    playerImg.height, //actual height
-    );
+    playerImg.height //actual height
+  );
 
-    if (keys.w.pressed && lastKey === 'w' 
-      || keys.ArrowUp.pressed && lastKey === 'ArrowUp') {
-      map.position.y += 3;
-    } else if (keys.s.pressed && lastKey === 's' 
-      || keys.ArrowDown.pressed && lastKey === 'ArrowDown') {
-      map.position.y -= 3;
-    } else if (keys.a.pressed && lastKey === 'a' 
-      || keys.ArrowLeft.pressed && lastKey === 'ArrowLeft') {
-      map.position.x += 3;
-    } else if (keys.d.pressed && lastKey === 'd' 
-      || keys.ArrowRight.pressed && lastKey === 'ArrowRight') {
-      map.position.x -= 3;
-    }
+  if (
+    (keys.w.pressed && lastKey === "w") ||
+    (keys.ArrowUp.pressed && lastKey === "ArrowUp")
+  ) {
+    map.position.y += 3;
+  } else if (
+    (keys.s.pressed && lastKey === "s") ||
+    (keys.ArrowDown.pressed && lastKey === "ArrowDown")
+  ) {
+    map.position.y -= 3;
+  } else if (
+    (keys.a.pressed && lastKey === "a") ||
+    (keys.ArrowLeft.pressed && lastKey === "ArrowLeft")
+  ) {
+    map.position.x += 3;
+  } else if (
+    (keys.d.pressed && lastKey === "d") ||
+    (keys.ArrowRight.pressed && lastKey === "ArrowRight")
+  ) {
+    map.position.x -= 3;
+  }
 }
 animate();
